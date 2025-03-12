dfcf097a2837d15c45467a894523e67726124d2b
Dragan Gjorgjiev <dragan.gjorgjiev@skopje.prospective.ch>
2025-03-12 11:35:29 +0100

    Fix tests by including globals in the makefile, replace deprecated fail_unless with ck_assert_msg

--- tests/check_snowflake.c
+++ tests/check_snowflake.c
@@ -7,18 +7,18 @@
 
 START_TEST(test_pools_empty) {
     int test = snowflake_init(0, 10);
-    fail_unless(test == 1, "Snowflake failed to init");
-    fail_unless(snowflake_global_state.region_id == 10L, "Region ID initialized incorrectly in the global state");
-    fail_unless(snowflake_global_state.worker_id == 10L, "Worker ID initialized incorrectly in the global state");
-    fail_unless(snowflake_global_state.seq_max == (1L << SNOWFLAKE_SEQUENCE_BITS) - 1, "Sequence Max initialized incorrectly in the global state");
-    fail_unless(snowflake_global_state.seq == 0L, "Sequence number initialized incorrectly in the global state");
-    fail_unless(snowflake_global_state.time == 0L, "Time initialized incorrectly in the global state");
-    fail_unless(app_stats.seq_cap == snowflake_global_state.seq_max, "app_stats.seq_cap != snowflake_global_state.seq_max");
-    fail_unless(app_stats.waits == 0L, "Waits counter not initialized correctly");
-    fail_unless(app_stats.seq_max == 0L, "Max generated sequence number not initialized correctly");
-    fail_unless(app_stats.ids == 0L, "ID generation counter not initialized correctly");
-    fail_unless(app_stats.region_id == snowflake_global_state.region_id, "app_stats.region_id != snowflake_global_state.region_id");
-    fail_unless(app_stats.worker_id == snowflake_global_state.worker_id, "app_stats.region_id != snowflake_global_state.region_id");
+    ck_assert_msg(test == 1, "Snowflake failed to init");
+    ck_assert_msg(snowflake_global_state.region_id == 10L, "Region ID initialized incorrectly in the global state");
+    ck_assert_msg(snowflake_global_state.worker_id == 10L, "Worker ID initialized incorrectly in the global state");
+    ck_assert_msg(snowflake_global_state.seq_max == (1L << SNOWFLAKE_SEQUENCE_BITS) - 1, "Sequence Max initialized incorrectly in the global state");
+    ck_assert_msg(snowflake_global_state.seq == 0L, "Sequence number initialized incorrectly in the global state");
+    ck_assert_msg(snowflake_global_state.time == 0L, "Time initialized incorrectly in the global state");
+    ck_assert_msg(app_stats.seq_cap == snowflake_global_state.seq_max, "app_stats.seq_cap != snowflake_global_state.seq_max");
+    ck_assert_msg(app_stats.waits == 0L, "Waits counter not initialized correctly");
+    ck_assert_msg(app_stats.seq_max == 0L, "Max generated sequence number not initialized correctly");
+    ck_assert_msg(app_stats.ids == 0L, "ID generation counter not initialized correctly");
+    ck_assert_msg(app_stats.region_id == snowflake_global_state.region_id, "app_stats.region_id != snowflake_global_state.region_id");
+    ck_assert_msg(app_stats.worker_id == snowflake_global_state.worker_id, "app_stats.region_id != snowflake_global_state.region_id");
     
 }
 
